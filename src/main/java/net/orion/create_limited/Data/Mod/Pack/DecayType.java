c099331184886f92f3c8e15547450a8a87ae3436
Orion <bferraz1313@gmail.com>
2025-06-04 14:21:17 +0200

    transfer

--- src/main/java/net/orion/create_limited/Data/Mod/Pack/DecayType.java
+++ src/main/java/net/orion/create_limited/Data/Mod/Pack/DecayType.java
@@ -2,16 +2,6 @@ package net.orion.create_limited.Data.Mod.Pack;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.orion.create_limited.Data.Constants.CommonConstants;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -24,14 +14,13 @@ public record DecayType(Map<String, DecayEntry> blockDecayValueMap) {
         ).apply(instance, DecayType::new)
     );
 
-    public record DecayEntry(String replaceWith, TagOrItemOrItemListType repairItems, long decayTime, int decayPhases, DecayEnumType decayEnumType, int amountConsumed) {
+    public record DecayEntry(String replaceWith, TagOrItemOrItemListType repairItems, long decayTime, int decayPhases, int amountConsumed) {
         public static final Codec<DecayEntry> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
                 Codec.STRING.optionalFieldOf("replaceWith", "minecraft:air").forGetter(DecayEntry::replaceWith),
                 TagOrItemOrItemListType.CODEC.fieldOf("repairItems").forGetter(DecayEntry::repairItems),
                 Codec.LONG.fieldOf("decayProgressTime").forGetter(DecayEntry::decayTime),
                 Codec.INT.optionalFieldOf("decayPhases", 10).forGetter(DecayEntry::decayPhases),
-                DecayEnumType.CODEC.optionalFieldOf("type", DecayEnumType.SINGLE).forGetter(DecayEntry::decayEnumType),
                 Codec.INT.optionalFieldOf("amountConsumed", 1).forGetter(DecayEntry::amountConsumed)
             ).apply(instance, DecayEntry::new)
         );
@@ -40,29 +29,5 @@ public record DecayType(Map<String, DecayEntry> blockDecayValueMap) {
     public record TagOrItemOrItemListType(List<String> items) {
         public static final Codec<TagOrItemOrItemListType> CODEC =
                 Codec.list(Codec.STRING).xmap(TagOrItemOrItemListType::new, TagOrItemOrItemListType::items);
-    }
-
-    public enum DecayEnumType {
-        SINGLE("single"),
-        MULTI("multi"),
-        COMPONENT("component");
-
-        public final String type;
-
-        DecayEnumType(String type) {
-            this.type = type.toLowerCase();
-        }
-
-        public static final Codec<DecayEnumType> CODEC = Codec.STRING.xmap(
-            DecayEnumType::fromString,
-            decayEnumType -> decayEnumType.type
-        );
-
-        private static DecayEnumType fromString(String name) {
-            return Arrays.stream(values())
-                    .filter(e -> e.type.equalsIgnoreCase(name))
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("Unknown DecayEnumType: " + name));
-        }
     }
 }
